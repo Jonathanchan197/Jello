5699a13b93981706f0c992ea1ccd3962cd642576
Jonathan Chan <Jonathanchan197@gmail.com>
2022-09-06 13:48:41 +1000

    trello backend connected

--- src/components/Trello.jsx
+++ src/components/Trello.jsx
@@ -1,149 +1,124 @@
-import React, { useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { v4 as uuidv4 } from 'uuid';
+import React from "react";
+import initialData from "../initial-data";
+import Column from "./Column";
+import { DragDropContext } from "react-beautiful-dnd";
+import styled from "styled-components";
+import { db } from "../firebase";
 
-const itemsFromBackend = [
-  { id: uuidv4(), content: "First task" },
-  { id: uuidv4(), content: "Second task" },
-  { id: uuidv4(), content: "Third task" },
-  { id: uuidv4(), content: "Fourth task" },
-  { id: uuidv4(), content: "Fifth task" }
-];
+const Container = styled.div`
+    display: flex;
+`;
 
-const columnsFromBackend = {
-  [uuidv4()]: {
-    name: "Requested",
-    items: itemsFromBackend
-  },
-  [uuidv4()]: {
-    name: "To do",
-    items: []
-  },
-  [uuidv4()]: {
-    name: "In Progress",
-    items: []
-  },
-  [uuidv4()]: {
-    name: "Done",
-    items: []
-  }
-};
 
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
+const Button = styled.button`
+background: transparent;
+  border-radius: 3px;
+  border: 2px solid black;
+  color: black;
+  margin: 0 1em;
+  height: 30px;
+  margin-top: 8px;
+  margin-left: 3px;
+  `
 
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    });
-  }
-};
+class Trello extends React.Component {
+    state = {
+        columnOrder: [],
+        columns: {},
+        tasks: {}
+    }
 
-function Trello() {
-  const [columns, setColumns] = useState(columnsFromBackend);
-  return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext
-        onDragEnd={result => onDragEnd(result, columns, setColumns)}
-      >
-        {Object.entries(columns).map(([columnId, column], index) => {
-          return (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center"
-              }}
-              key={columnId}
-            >
-              <h2>{column.name}</h2>
-              <div style={{ margin: 8 }}>
-                <Droppable droppableId={columnId} key={columnId}>
-                  {(provided, snapshot) => {
-                    return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver
-                            ? "lightblue"
-                            : "lightgrey",
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500
-                        }}
-                      >
-                        {column.items.map((item, index) => {
-                          return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={item.id}
-                              index={index}
-                            >
-                              {(provided, snapshot) => {
-                                return (
-                                  <div
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      userSelect: "none",
-                                      padding: 16,
-                                      margin: "0 0 8px 0",
-                                      minHeight: "50px",
-                                      backgroundColor: snapshot.isDragging
-                                        ? "#263B4A"
-                                        : "#456C86",
-                                      color: "white",
-                                      ...provided.draggableProps.style
-                                    }}
-                                  >
-                                    {item.content}
-                                  </div>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Droppable>
-              </div>
-            </div>
-          );
-        })}
-      </DragDropContext>
-    </div>
-  );
+    componentDidMount() {
+        console.log('mounted')
+        db.collection("Workspaces")
+            .doc("aRzyA8nDdTpij51kLFMr")
+            .get()
+            .then(res => {
+                this.setState(res.data())
+            })
+            .catch(error => console.log(error))
+    }
+
+    onDragEnd = result => {
+        const {destination, source, draggableId} = result;
+
+        if (!destination) {
+            return;
+        }
+
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index 
+        ) {
+            return;
+        }
+
+        const start = this.state.columns[source.droppableId];
+        const finish = this.state.columns[destination.droppableId];
+
+        if (start === finish){
+            const newTaskIds = Array.from(start.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        const newColumn = {
+            ...start,
+            taskIds: newTaskIds,
+        };
+
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newColumn.id]: newColumn,
+            },
+        };
+
+        this.setState(newState)
+        db.collection("Workspaces").doc("aRzyA8nDdTpij51kLFMr").set(newState);
+        return;
+        }
+
+        const startTaskIds = Array.from(start.taskIds);
+        startTaskIds.splice(source.index, 1);
+        const newStart = {
+            ...start,
+            taskIds: startTaskIds,
+        };
+
+        const finishTaskIds = Array.from(finish.taskIds);
+        finishTaskIds.splice(destination.index, 0, draggableId)
+        const newFinish = {
+            ...finish,
+            taskIds: finishTaskIds,
+        }
+
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newStart.id]: newStart,
+                [newFinish.id]: newFinish,
+            },
+        };
+        this.setState(newState);
+        db.collection("Workspaces").doc("aRzyA8nDdTpij51kLFMr").set(newState);
+    };
+
+    render() {
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                <Container>
+                {this.state.columnOrder.map(columnId => {
+                    const column = this.state.columns[columnId];
+                    const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+                return <Column key={column.id} column={column} tasks={tasks} />;
+            })}
+            <Button>Add List</Button>
+        </Container>
+        </DragDropContext>
+        )
+    }
 }
 
-export default Trello;
+export default Trello
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
- 
-const firebaseConfig = {
-    apiKey: "AIzaSyDQucc9xQNzVH0Xknmn83D5Bmam9gy6ErI",
-    authDomain: "jello-75423.firebaseapp.com",
-    projectId: "jello-75423",
-    storageBucket: "jello-75423.appspot.com",
-    messagingSenderId: "878577750624",
-    appId: "1:878577750624:web:ca80f2789e3cbc7c1f6905",
-    measurementId: "G-G3X9K816CB",
-  };
+import "firebase/compat/firestore";
+import firebase from "firebase/compat/app";
 
-  const app = initializeApp(firebaseConfig);
-  
-export const auth = getAuth(app);   
-export default app
+const firebaseConfig = {
+  apiKey: "AIzaSyDQucc9xQNzVH0Xknmn83D5Bmam9gy6ErI",
+  authDomain: "jello-75423.firebaseapp.com",
+  projectId: "jello-75423",
+  storageBucket: "jello-75423.appspot.com",
+  messagingSenderId: "878577750624",
+  appId: "1:878577750624:web:ca80f2789e3cbc7c1f6905",
+  measurementId: "G-G3X9K816CB",
+};
+
+const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
+export const auth = getAuth(app);
+export default app;
