4b7343f08bad075ce4cd554ce1e92f81960dbac4
Jonathan Chan <Jonathanchan197@gmail.com>
2022-09-08 22:36:13 +1000

    using hashrouters for deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import Navigation from "./Navigation";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
